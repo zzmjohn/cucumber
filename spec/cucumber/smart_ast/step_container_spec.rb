890f487a8f17fe74789b45432a99000915ead45d
Mike Sassak <msassak@gmail.com>
2009-11-18 16:46:16 -0500

    More specs

--- spec/cucumber/smart_ast/step_container_spec.rb
+++ spec/cucumber/smart_ast/step_container_spec.rb
@@ -15,19 +15,34 @@ module Cucumber
       end
 
       it "should include the table argument to a step" do
-        @step_container.step("Given", "a table", 2)
         rows = [%w(a b), %w(c d)]
+        @step_container.step("Given", "a table", 2)
         @step_container.table(rows, 3)
         @step_container.should have(1).steps
-        step = @step_container.steps.first
-        step.argument.raw.should == rows
+        @step_container.steps[0].argument.raw.should == rows
       end
 
       it "should include py_string argument to a step" do
+        content = "Long string"
         @step_container.step("Given", "a py_string", 2)
-        py_string = "Long string"
-        @step_container.py_string(1, py_string, 3)
-        @step_container.steps.first.argument.to_s.should == "Long string"
+        @step_container.py_string(1, content, 3)
+        @step_container.steps[0].argument.to_s.should == content
+      end
+
+      it "should handle many steps with arguments" do
+        rows = [[1, 2]]
+        content = "PyString!"
+
+        @step_container.step("Given", "a normal step", 2)
+        @step_container.step("When", "a table", 3)
+        @step_container.table(rows, 4)
+        @step_container.step("Then", "a py_string", 5)
+        @step_container.py_string(1, content, 6)
+        @step_container.step("And", "another normal step", 7)
+
+        @step_container.should have(4).steps
+        @step_container.steps[1].argument.raw.should == rows
+        @step_container.steps[2].argument.to_s.should == content
       end
     end
   end
