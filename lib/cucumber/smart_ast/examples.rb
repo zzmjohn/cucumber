7cd5aec2bc9ff9f1fcbc1b94eba79672dda5f04e
Mike Sassak <msassak@gmail.com>
2009-11-18 16:16:12 -0500

    More flail

--- lib/cucumber/smart_ast/examples.rb
+++ lib/cucumber/smart_ast/examples.rb
@@ -3,7 +3,10 @@ require 'cucumber/smart_ast/step_container'
 module Cucumber
   module SmartAst
     class Examples < StepContainer
-      include Tables
+      attr_reader :argument
+      def table(rows, line)
+        @argument = Table.new(rows, line)
+      end
     end
   end
 end
--- lib/cucumber/smart_ast/step.rb
+++ lib/cucumber/smart_ast/step.rb
@@ -1,9 +1,17 @@
 module Cucumber
   module SmartAst
-    class Step
-      attr_accessor :argument
+    class Step 
+      attr_reader :argument
       def initialize(adverb, body, line)
         @adverb, @body, @line = adverb, body, line
+      end
+
+      def table(rows, line)
+        @argument = Table.new(rows, line)
+      end
+
+      def py_string(start_col, content, line)
+        @argument = PyString.new(start_col, content, line)
       end
     end
   end
